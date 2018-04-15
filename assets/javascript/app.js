47c1876733f0415923a97cc1ebbda560aed6ff9c
ryanoliversotto <ryan.sotto@outlook.com>
2018-04-15 14:28:35 -0500

    Easy Trivia. Adding inline jQuery to write questions and radio button groups

---
+++ assets/javascript/app.js
@@ -0,0 +1,43 @@
+/*___________________Technology Division____________________
+Languages used: jQuery, Javascript, HTML, CSS
+Option One: Basic Triva Quiz (Timed Form)
+
+_____________________Project Requirements___________________
+You'll create a trivia form with multiple choice or true/false options (your choice).
+
+The player will have a limited amount of time to finish the quiz. 
+
+
+The game ends when the time runs out. The page will reveal the number of questions that players answer correctly and incorrectly.
+
+
+Don't let the player pick more than one answer per question.
+Don't forget to include a countdown timer.
+
+_____________________Pseudocode Division______________________
+One the landing page there will be a simple header and button.
+Upon the submit event of the button, the quiz will start.
+
+Division1 will display a timer.
+
+Division2 will contain 2 nested for loops.
+Loop 1 for the length of the array of questions
+    $write the html for the questions to the page
+        Loop 2 for the length of the radio buttons
+            $write the html of the radio buttons to the page
+At the end of the loop $write a submit button
+The game will end upon the countdown = 0 or if user hits submit.
+$write the results to the html page
+Provide option to reset the game and start over.
+
+3 variables to
+1) Right 
+2) Wrong 
+3) Unanswered 
+4) Hold the user selection
+
+An object with 2 arrays
+Question 1 array
+Radio Buttons 2 Array
+Right Answer
+*/
