e9359a6c71e01828f72a7ac0d4e224c4574e1bca
ryanoliversotto <ryan.sotto@outlook.com>
2018-04-16 21:18:49 -0500

    Added css and removed testing files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -41,3 +41,145 @@ Question 1 array
 Radio Buttons 2 Array
 Right Answer
 */
+var userPick = [];
+var correctAnswers = 0;
+var wrongAnswers = 0;
+var missedAnswers = 0;
+var timeDisplay;
+var counter = 60;
+var intervalID;
+var questions = [{
+    question: "What was is Shaggy's real first name?",
+    choices: ["Michael", "Johnathan", "Norville", "Elmer"],
+    answer: 2
+},
+{
+    question: "What breed of dog is Scooby Doo?",
+    choices: ["Great Dane", "Irish Wolfhound", "German Shepard", "Bloodhound"],
+    answer: 0
+},
+{
+    question: "Which of the Scooby Doo gang has the catchphrase 'JINKIES!'",
+    choices: ["Fred", "Velma", "Daphne", "Scooby Doo"],
+    answer: 1
+},
+{
+    question: "What is the last name of Fred?",
+    choices: ["Johnson", "Smith", "Jones", "Appleton"],
+    answer: 2
+},
+{
+    question: "What is the name of the van the Scooby gang travels in?",
+    choices: ["Mystery Busters", "Mystery Machine", "Ghost Adventures", "BloodHound Gang"],
+    answer: 1
+},
+{
+    question: "In what year did Scooby Doo debut on national television?",
+    choices: ["1972", "1965", "1969", "1970"],
+    answer: 3
+},
+{
+    question: "Which television network was the first to air Scooby Doo?",
+    choices: ["CBS", "ABC", "NBC", "PBS"],
+    answer: 0
+},
+{
+    question: "1985s Thirteen Ghosts of Scooby Doo featured which new character?",
+    choices: ["Bill the Cat", "Scrappy Doo", "Scooby Dee", "Vincent Van Ghoul"],
+    answer: 3
+},
+{
+    question: "What is the full name of Scooby Doo?",
+    choices: ["Prince Scooby", "Scooby Dooby Do", "Scoobert", "Scooby Rex"],
+    answer: 2
+},
+{
+    question: "What was the catchphrase of Scrappy Doo?",
+    choices: ["Puppy Power", "Lemme at him!", "Clobbering Time!", "Paw Paw Paw Punch!"],
+    answer: 0
+}
+];
+
+for ( var i = 0; i < questions.length; i++){
+    userPick[i] = null;
+}
+
+//Optional sounds
+let audioClick = new Audio("assets/sounds/mouse_click.wav");
+
+//console.log("Questions length ", questions.length);
+
+$(document).ready(function () {
+
+    $("#startGame").click(function () {
+        audioClick.play();
+        intervalID = setInterval(decrement, 1000);
+        writeQuestions();
+        $("#startGame").hide();
+        writeSubmitButton();
+
+        $("#submitQuiz").click(function () {
+            //alert("I was clicked!");
+            audioClick.play();
+            showResults();
+        });
+
+        $("input").click(function(){
+            audioClick.play();
+            userPick[this.name]=this.value;
+        });
+
+
+    });
+});
+
+
+function writeQuestions() {
+    for (var i = 0; i < questions.length; i++) {
+        $("#formQuiz").append(questions[i].question + "</br></br>");
+        for (var x = 0; x < questions[i].choices.length; x++) {
+            $("#formQuiz").append("<label class='radio-inline'><input value='" + x + "' type='radio' name='" + i + "'>" + questions[i].choices[x] + "</label><br/><br/>");
+        }
+    }
+}
+
+function writeSubmitButton() {
+    $("#formSubmit").append("<button id='submitQuiz' class='btn btn-primary btn-lg'>Submit</button>");
+}
+
+
+function decrement() {
+    counter--;
+    $("#timeRemaining").html("<h2><mark>" + counter + " seconds remaining.</mark></h2>");
+    if (counter === 0) {
+        alert("Time Up!");
+        stop();
+        //Do additional logic and process the quiz results
+        showResults();
+    }
+}
+
+function showResults() {
+    $("#formQuiz").hide();
+    $("#timeRemaining").hide();
+    $("#submitQuiz").hide();
+    for (i = 0; i < questions.length; i++) {
+        if (questions[i].answer == userPick[i]) {
+            correctAnswers++;
+        }
+        else if (userPick[i] === null) {
+            missedAnswers++;
+        }
+        else {
+            wrongAnswers++;
+        }
+    }
+    $("#quizResults").append("<p>ALL DONE!</p>");
+    $("#quizResults").append("<p>Correct Answers: " + correctAnswers + "</p>");
+    $("#quizResults").append("<p>Incorrect Answers: " + wrongAnswers + "</p>");
+    $("#quizResults").append("<p>Unanswered: " + missedAnswers + "</p>");
+}
+
+function stop(){
+    clearInterval(intervalID);
+}
